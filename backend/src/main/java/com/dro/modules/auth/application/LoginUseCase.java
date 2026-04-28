bbaf23b4fb50cf2b749640f488a587bcf24f0149
Rafael Fonseca Hermes Azevedo <rafael.azevedo.dev@gmail.com>
2026-04-28 00:13:17 -0300

    feat(auth): implement custom exceptions for authentication errors

--- backend/src/main/java/com/dro/modules/auth/application/LoginUseCase.java
+++ backend/src/main/java/com/dro/modules/auth/application/LoginUseCase.java
@@ -2,6 +2,7 @@ package com.dro.modules.auth.application;
 
 import com.dro.modules.auth.api.dto.LoginRequest;
 import com.dro.modules.auth.api.dto.LoginResponse;
+import com.dro.modules.auth.domain.exception.InvalidCredentialsException;
 import com.dro.modules.player.domain.Player;
 import com.dro.modules.player.infra.PlayerRepository;
 import lombok.RequiredArgsConstructor;
@@ -20,10 +21,10 @@ public class LoginUseCase {
     public LoginResponse execute(LoginRequest request) {
 
         Player player = repository.findByEmail(request.email())
-                .orElseThrow(() -> new RuntimeException("Invalid credentials"));
+                .orElseThrow(InvalidCredentialsException::new);
 
         if (!passwordEncoder.matches(request.password(), player.getPassword())) {
-            throw new RuntimeException("Invalid credentials");
+            throw new InvalidCredentialsException();
         }
 
         // Token fake por enquanto
